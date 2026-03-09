780172b488392da16623657411295c1a7e3de678
eskopp <info@erik-skopp.de>
2026-03-09 23:27:43 +0100

    add aur helpers setup step
    - add step 0008 to install paru and yay from the AUR
    - use paru-bin and yay-bin for a simpler bootstrap path
    - build AUR packages as the normal user with makepkg

---
+++ steps/0008-aur-helpers.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+AUR_BUILD_ROOT="${AUR_BUILD_ROOT:-$HOME/.cache/arch-setup/aur}"
+
+msg() {
+  printf '\033[1;34m[INFO]\033[0m %s\n' "$*"
+}
+
+die() {
+  printf '\033[1;31m[ERROR]\033[0m %s\n' "$*" >&2
+  exit 1
+}
+
+require_not_root() {
+  [[ "${EUID}" -ne 0 ]] || die "Run this step as a normal user, not as root."
+}
+
+ensure_dependencies() {
+  msg "Installing AUR build dependencies"
+  sudo pacman -S --needed --noconfirm base-devel git
+}
+
+build_and_install_aur_pkg() {
+  local pkgbase="$1"
+  local repo_url="https://aur.archlinux.org/${pkgbase}.git"
+  local workdir="$AUR_BUILD_ROOT/$pkgbase"
+
+  mkdir -p "$AUR_BUILD_ROOT"
+
+  if [[ -d "$workdir/.git" ]]; then
+    msg "Updating existing AUR checkout: $pkgbase"
+    git -C "$workdir" fetch --all --prune
+    git -C "$workdir" reset --hard origin/master
+    git -C "$workdir" clean -fdx
+  else
+    msg "Cloning AUR package: $pkgbase"
+    git clone "$repo_url" "$workdir"
+  fi
+
+  msg "Building and installing $pkgbase"
+  (
+    cd "$workdir"
+    makepkg -si --noconfirm --needed
+  )
+}
+
+main() {
+  require_not_root
+  ensure_dependencies
+
+  build_and_install_aur_pkg "paru-bin"
+  build_and_install_aur_pkg "yay-bin"
+
+  msg "0008 AUR helpers setup completed"
+}
+
+main "$@"
