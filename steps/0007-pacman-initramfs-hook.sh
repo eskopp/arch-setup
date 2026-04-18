f7fac93b0df0c0ab05ad81ed5609a8744e4ca5d1
Erik Skopp <info@erik-skopp.de>
2026-04-18 21:28:21 +0200

    Disable Tablet

--- steps/0007-pacman-initramfs-hook.sh
+++ steps/0007-pacman-initramfs-hook.sh
@@ -25,6 +25,15 @@ Depends = mkinitcpio
 HOOK
 
   msg "Custom pacman hook installed at /etc/pacman.d/hooks/95-mkinitcpio-extra.hook"
+
+sudo mkdir -p /etc/libinput
+
+sudo tee /etc/libinput/local-overrides.quirks >/dev/null <<'EOF'
+[Lenovo Yoga disable broken tablet mode switch]
+MatchName=Lenovo Yoga Tablet Mode Control switch
+ModelTabletModeSwitchUnreliable=1
+EOF
+
 }
 
 main "$@"
